33ad4482164d030125d0c88d307e1fc10d6107bb
Joao Gabriel Chiarello <joao.chiarello@angellira.com.br>
2024-10-22 15:37:00 -0300

    Adicionado firebase ao projeto.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -50,4 +52,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.5.0"))
+
 }
