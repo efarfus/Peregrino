d46dc1d6a65e5f688e32c9e6d7cc572fb651a127
Eduardo Farfus <eduardofarfus@gmail.com>
2024-11-08 10:03:16 -0300

    Iniciado layout do PerfilActivity

--- app/src/main/java/com/angellira/peregrino/PerfilActivity.kt
+++ app/src/main/java/com/angellira/peregrino/PerfilActivity.kt
@@ -1,30 +1,26 @@
 package com.angellira.peregrino
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.angellira.peregrino.databinding.ActivityMainBinding
+import com.angellira.peregrino.databinding.ActivityPerfilBinding
 
-class MainActivity : AppCompatActivity() {
+private lateinit var binding: ActivityPerfilBinding
 
-    private lateinit var binding: ActivityMainBinding
-
+class PerfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setupView()
-        binding.buttonPerfil.setOnClickListener {
-            startActivity(Intent(this, PerfilActivity::class.java))
-        }
+
 
     }
 
     private fun setupView() {
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityPerfilBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
