460912dc62d09bb898d66920ed0624a3373438db
Eduardo Farfus <eduardofarfus@gmail.com>
2024-11-11 10:34:49 -0300

    Ajustado bindings de novas páginas

--- app/src/main/java/com/angellira/peregrino/VeiculosActivity.kt
+++ app/src/main/java/com/angellira/peregrino/VeiculosActivity.kt
@@ -5,12 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.angellira.peregrino.databinding.ActivityMainBinding
+import com.angellira.peregrino.databinding.ActivityVeiculosBinding
 
 class VeiculosActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityVeiculosBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupView()
+    }
+
+    private fun setupView() {
         enableEdgeToEdge()
-        setContentView(R.layout.activity_veiculos)
+        binding = ActivityVeiculosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
